a7329aa6639e43f048188c7fdd2f449ee603336b
Tomáš Klapka <tomas@klapka.cz>
2019-02-07 20:34:38 +0100

    add license to every file

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
+// LICENSE
+// This software is free for use and redistribution while including this
+// license notice, unless:
+// 1. is used for commercial or non-personal purposes, or
+// 2. used for a product which includes or associated with a blockchain or other
+// decentralized database technology, or
+// 3. used for a product which includes or associated with the issuance or use
+// of cryptographic or electronic currencies/coins/tokens.
+// On all of the mentioned cases, an explicit and written permission is required
+// from the Author (Ohad Asor).
+// Contact ohad@idni.org for requesting a permission. This license may be
+// modified over time by the Author.
+
+// This is a Javascript rewrite of TML by Tomáš Klapka <tomas@klapka.cz>
+
 const { dict, lp } = require("./tml");
 const { node, bdds, bdds_base, op_exists, options } = require("./bdds");
 
